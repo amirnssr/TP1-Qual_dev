c97e51a69c8bc08166356192b83fd07ca881317f
Amir Nassiri <22300894@me.uvsq.fr>
2025-01-27 12:19:35 +0100

    documentation des classes inscriptionmonocoque et inscriptionVoilier

--- untitled/src/InscriptionMonocoque.java
+++ untitled/src/InscriptionMonocoque.java
@@ -1,23 +1,35 @@
+/**
+ * La classe InscriptionMonocoque représente une inscription spécifique pour un voilier monocoque.
+ * Cette classe étend InscriptionVoilier et implémente le pattern Factory Method pour créer des instances
+ * de Monocoque ou Multicoque.
+ */
 public class InscriptionMonocoque extends InscriptionVoilier {
 
-
-   @Override
-   public Voilier factoryMethod(String nom, SuivreRoute suivreRoute,String nomSkipper,EnumClasse enumClasse ){
-
-
-        if (enumClasse == EnumClasse.EnumMono){
-            return new Monocoque(nom,suivreRoute,nomSkipper,enumClasse);
+    /**
+     * Méthode de fabrique pour créer un voilier en fonction de sa classe.
+     *
+     * @param nom le nom du voilier
+     * @param suivreRoute la stratégie de suivi de route
+     * @param nomSkipper le nom du skipper
+     * @param enumClasse la classe du voilier (monocoque ou multicoque)
+     * @return une instance de Monocoque si la classe est EnumMono, sinon une instance de Multicoque
+     */
+    @Override
+    public Voilier factoryMethod(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClasse enumClasse) {
+        if (enumClasse == EnumClasse.EnumMono) {
+            return new Monocoque(nom, suivreRoute, nomSkipper, enumClasse);
+        } else {
+            return new Multicoque(nom, suivreRoute, nomSkipper, enumClasse);
         }
-        else  {
-            return new Multicoque(nom,suivreRoute,nomSkipper,enumClasse);
-        }
-
-
-
     }
 
+    /**
+     * Enregistre un voilier dans la liste des voiliers inscrits.
+     *
+     * @param voilier le voilier à enregistrer
+     */
     @Override
-    public void enregistrement(Voilier voilier){
-       listVoilier.add(voilier);
-   }
+    public void enregistrement(Voilier voilier) {
+        listVoilier.add(voilier);
+    }
 }
--- untitled/src/InscriptionVoilier.java
+++ untitled/src/InscriptionVoilier.java
@@ -1,17 +1,43 @@
 import java.util.List;
 
+/**
+ * La classe abstraite InscriptionVoilier représente le processus d'inscription d'un voilier.
+ * Elle définit une méthode de fabrique abstraite pour créer des instances de Voilier et
+ * une méthode pour enregistrer les voiliers.
+ */
 public abstract class InscriptionVoilier {
+    /**
+     * La liste des voiliers inscrits.
+     */
     static List<Voilier> listVoilier;
 
-    public abstract Voilier factoryMethod(String nom, SuivreRoute suivreRoute,String nomSkipper,EnumClasse classe);
+    /**
+     * Méthode de fabrique abstraite pour créer un voilier.
+     *
+     * @param nom le nom du voilier
+     * @param suivreRoute la stratégie de suivi de route
+     * @param nomSkipper le nom du skipper
+     * @param classe la classe du voilier
+     * @return une instance de Voilier
+     */
+    public abstract Voilier factoryMethod(String nom, SuivreRoute suivreRoute, String nomSkipper, EnumClasse classe);
 
-    void enregistrement(Voilier voilier){
+    /**
+     * Enregistre un voilier dans la liste des voiliers inscrits.
+     *
+     * @param voilier le voilier à enregistrer
+     */
+    void enregistrement(Voilier voilier) {
         listVoilier.add(voilier);
     }
 
+    /**
+     * Retourne une représentation sous forme de chaîne de caractères de la liste des voiliers inscrits.
+     *
+     * @return une chaîne de caractères représentant la liste des voiliers inscrits
+     */
     @Override
     public String toString() {
         return listVoilier.toString();
     }
-
 }
